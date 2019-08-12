b4deb78378bb8bf1c5274f8742ab5790f9bf3216
Kamil <53897136+Kamil-Jankowski@users.noreply.github.com>
2019-08-12 20:29:26 +0200

    Update DooBee.java

--- DooBee.java
+++ DooBee.java
@@ -1,13 +1,13 @@
 class DooBee{
-public static void main(String[] args){
-int x = 1;
-while(x<3){
-System.out.print("Doo");
-System.out.print("Bee");
-x++;
-}
-if(x==3){
-System.out.print("Do");
-}
-}
+        public static void main(String[] args){
+                int x = 1;
+                while (x  < 3) {
+                        System.out.print("Doo");
+                        System.out.print("Bee");
+                        x++;
+                }
+                if(x == 3) {
+                        System.out.print("Do");
+                }
+        }
 }
